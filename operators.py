778ee45bbb348cbaa227a5d8930113a8031cce70
Patrick <huangpatrick16777216@gmail.com>
2020-11-12 15:49:00 -0800

    Added connection operators.

--- operators.py
+++ operators.py
@@ -15,8 +15,29 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
+import socket
 import bpy
 from bpy.types import Operator
+from .connection import Client, Server
+
+status = None
+conn = None
+
+
+class RENDERFARM_OT_StartServer(Operator):
+    bl_label = "Start Server"
+    bl_description = "Start server and allow others to connect."
+    bl_idname = "local_render_farm.start_server"
+
+    def execute(self, context):
+        global status, conn
+        settings = context.scene.local_render_farm
+
+        conn = Server(socket.gethostbyname(socket.gethostname()), 5555)
+        conn.Start()
+
+        status = "WAITING"
+        return {"FINISHED"}
 
 
 class RENDERFARM_OT_Connect(Operator):
@@ -25,15 +46,25 @@ class RENDERFARM_OT_Connect(Operator):
     bl_idname = "local_render_farm.connect"
 
     def execute(self, context):
+        global status, conn
         settings = context.scene.local_render_farm
+
+        conn = Client(settings.serverIp, 5555)
+
+        status = "WAITING"
         return {"FINISHED"}
 
 
 classes = (
+    RENDERFARM_OT_StartServer,
     RENDERFARM_OT_Connect,
 )
 
 def register():
+    global status, conn
+    status = "UNCONN"
+    conn = None
+
     for cls in classes:
         bpy.utils.register_class(cls)
 
