ed96f91fd63d5014da49a26e67347f30f8d21240
Patrick <huangpatrick16777216@gmail.com>
2020-11-12 17:36:20 -0800

    Display output dir.

--- ui.py
+++ ui.py
@@ -45,6 +45,7 @@ class RENDERFARM_PT_Main(Panel):
                 layout.label(text="Waiting for clients.")
                 layout.label(text=f"Your local IP is {socket.gethostbyname(socket.gethostname())}")
                 layout.prop(settings, "serverRender")
+                layout.prop(settings, "outputDir")
                 layout.operator("local_render_farm.start_render")
                 layout.separator()
 
