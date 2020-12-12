d3b77c2748cb7cf617f52aadfa0b9c92689b2c5d
Patrick Huang <huangpatrick16777216@gmail.com>
2020-12-12 12:07:42 -0800

    Added props.

---
+++ server/props.py
@@ -0,0 +1,47 @@
+#  ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+import bpy
+from bpy.types import PropertyGroup
+from bpy.props import BoolProperty, IntProperty, FloatProperty, StringProperty, EnumProperty, PointerProperty
+
+
+class RenderServerProps(PropertyGroup):
+    frame_start: IntProperty(
+        name="Start",
+        description="Starting frame to render (inclusive)",
+        default=0
+    )
+
+    frame_end: IntProperty(
+        name="End",
+        description="Ending frame to render (inclusive)",
+        default=2000
+    )
+
+
+classes = (
+    RenderServerProps,
+)
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+def unregister():
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
